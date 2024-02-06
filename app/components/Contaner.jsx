8eb023f6f1c1d9c66ddcb9419ed52dc83f7d61e3
Siraj soliman <healthtartus@gmail.com>
2024-02-06 12:45:56 +0300

    s4 commit

--- app/components/Contaner.jsx
+++ app/components/Contaner.jsx
@@ -1,5 +1,5 @@
 const Container = ({children}) => {
-    return <div className="flex w-screen h-screen">{children}</div>
+    return <div className="flex w-screen h-screen overflow-scroll">{children}</div>
 }
  
 export default Container;
