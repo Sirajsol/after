e72ca15f4ddd470135d2a5db403b9431a407c089
Siraj soliman <healthtartus@gmail.com>
2025-01-06 13:38:13 +0300

    sec6jan

--- app/components/eventcard/EventCard.jsx
+++ app/components/eventcard/EventCard.jsx
@@ -13,8 +13,8 @@ useEffect(()=>{
     return <div className={Style.mainContainer}
     // onClick={()=>{setWt(true)}}
     >
-       {(wt||wait)&&(<div className="flex  justify-center items-center absolute top-[150px] bg-blue-900 w-[400px] h-[80px] left-[200px] text-white
-    text-[30px] px-[40px] shadow-black shadow-md rounded z-10">الرجاء الإنتظار</div>)} 
+       {/* {(wt||wait)&&(<div className="flex  justify-center items-center absolute top-[150px] bg-blue-900 w-[400px] h-[80px] left-[200px] text-white
+    text-[30px] px-[40px] shadow-black shadow-md rounded z-10">الرجاء الإنتظار</div>)}  */}
         <div className={Style.title}>{ev.title}</div>
         <div className={Style.img}>
             <Image src={ev.img} alt={"image"} fill className="w-full h-full object-contain"/>
